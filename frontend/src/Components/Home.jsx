d031b9dcb9776b74c35adb7d044319a611899719
haran2231 <103673702+haran2231@users.noreply.github.com>
2024-09-21 23:40:08 +0530

    Update Home.jsx

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -151,7 +151,7 @@ const Home = () => {
               const userEmail = auth.currentUser.email;
 
               try {
-                const response = await fetch('http://localhost:5000/add-contact', {
+                const response = await fetch('https://chatapp-dt22.onrender.com/add-contact', {
                   method: 'POST',
                   headers: { 'Content-Type': 'application/json' },
                   body: JSON.stringify({ userEmail, contactEmail }),
